ec8e71eaf89cc00725f0009b1ad8bdaabeb6f7d8
Adrian Brion <rave_921@yahoo.com>
2010-10-17 17:22:51 +0800

    A Commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FatFreeCrm::Application.config.secret_token = 'a891c8c9d530eaa7e3cda1d9b34a8dd3b4f408fe67c282d9572a5caaebd57770fb6807067e8a5699badf8a19082d6cd8e344817b2dd536dd17eccce54a2b1a67'
+FatFreeCrm::Application.config.secret_token = '2484c59f8bc1b2ef50193804149ac283ce06893bb0d2f14acaa4e6c2537abe7da6b2d205e88682356530b9284ceba79de9588ff748c042d25136bc61d3ecde50'
