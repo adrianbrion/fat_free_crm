52f156fa2eeab34623a85cee3e2e19f93a4ccabf
Adrian Brion <rave_921@yahoo.com>
2010-10-17 16:11:51 +0800

    ffcrm commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FatFreeCrm::Application.config.secret_token = '8f95645f175292f7f6003d5becb926e27319293564c897a2b26e105c66561080d875be26af2b15aa0600ef20a2e8d5f9de563e04ce4d17d15fb52d82e6c38576'
